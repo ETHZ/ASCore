4e12696b185750d1f42c91ea28d89b46f372cd84
stiegerb <>
2009-12-07 18:21:41 +0000

    Added L1 Trigger info, fixed minor glitches

--- test/ntupleproducer_cfg.py
+++ test/ntupleproducer_cfg.py
@@ -11,13 +11,13 @@ process.MessageLogger.cerr.NTP = cms.untracked.PSet(
     reportEvery = cms.untracked.int32(1)
     )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
 ### Running conditions #########################################################
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag="MC_31X_V3::All"
+process.GlobalTag.globaltag = "MC_31X_V3::All"
 
 ### b-tagging ##################################################################
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
@@ -33,16 +33,27 @@ process.impactParameterTagInfos.jetTracks = cms.InputTag("sisCone5JetTracksAssoc
 process.source = cms.Source("PoolSource",
 	# replace 'myfile.root' with the source file you want to use
 	fileNames = cms.untracked.vstring(
-		# This is from /TTJets-madgraph/Fall08_IDEAL_V11_redigi_v10/AODSIM:
-		# 'file:/data/14/stiegerb/SUSY/31X/ppMuXLoose_Summer09-MC_31X_V3-v1_GEN-SIM-RECO.root'
-		#'file:/data/14/stiegerb/SUSY/31X/ppEleX_Summer09-MC_31X_V3-v1_GEN-SIM-RECO.root'
-		# 'file:EleIsoTest.root'
-		# This is from /SUSY_LM0-sftsht/Summer08_IDEAL_V11_v1/GEN-SIM-RECO:
-		 'file:/data/14/stiegerb/SUSY/31X/ppMuXLoose_Summer09-MC_31X_V3-v1_GEN-SIM-RECO.root'
+		'file:/home/xv/stiegerb/FirstData/Run123596/bit40-123596-stdReco-GR09_P_V7-lumi1-68.root'
+		# 'file:/data/theofil08/Data/Beam09/bit40or41skim_expressPhysics_run123596_full.root'
+		# 'file:/home/xv/stiegerb/Skims/python/CollisionSkim_123596_1.root'
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/241D28BF-1EE2-DE11-9CF3-001617E30CC8.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/5865B623-1EE2-DE11-82E2-001D09F2545B.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/5E32F723-1EE2-DE11-ACC5-001D09F252E9.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/68BAA366-1FE2-DE11-A742-001617C3B66C.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/A6C66722-1EE2-DE11-BCFC-001D09F25208.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/D0ACE867-1FE2-DE11-AF2E-000423D94A20.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/CA74FE6C-1FE2-DE11-9C9A-001617E30D4A.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/761A0BBF-1EE2-DE11-8E1B-001617C3B76E.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/863F0D1D-20E2-DE11-9D98-001617C3B706.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/98B3021F-20E2-DE11-A8A9-0030487D0D3A.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/F25C511D-20E2-DE11-82D0-001617C3B6DE.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/ACA6611D-20E2-DE11-B804-001617E30CC8.root',
+		# 'file:/home/xv/stiegerb/FirstData/Run123592/80B9DACB-20E2-DE11-9526-001617C3B706.root'
+
 	),
 	duplicateCheckMode = cms.untracked.string("noDuplicateCheck")
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 # Output
 process.TFileService = cms.Service("TFileService",
 	fileName = cms.string("NTupleProducer.root"),
@@ -106,4 +117,3 @@ process.p = cms.Path(
     + process.eleIsoFromDeposits
     + process.analyze
     )
-
