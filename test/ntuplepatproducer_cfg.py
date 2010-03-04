69abe9eac9efafc9da1f5adfd85013ad44c73d24
fronga <>
2010-03-04 10:17:43 +0000

    Remove custom electron isolation. Remove cleaning from path.

--- test/ntuplepatproducer_cfg.py
+++ test/ntuplepatproducer_cfg.py
@@ -14,8 +14,8 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 ### Switch for type of run (data, MC) and reconstruction (RECO, PAT, PF) #######
-runon = 'data'
-#runon = 'MC31x'
+#runon = 'data'
+runon = 'MC31x'
 #runon = 'MC34x'
 #recoType = 'RECO'
 recoType = 'PAT'
@@ -28,10 +28,10 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 if runon=='data':
     process.GlobalTag.globaltag = "GR09_R_34X_V3::All"
 else:
-	# CMSSW_3_4_X:
-	process.GlobalTag.globaltag = "MC_3XY_V18::All"
-	# CMSSW_3_3_X:
-	# process.GlobalTag.globaltag = "MC_31X_V3::All"
+    # CMSSW_3_4_X:
+    process.GlobalTag.globaltag = "MC_3XY_V18::All"
+    # CMSSW_3_3_X:
+    # process.GlobalTag.globaltag = "MC_31X_V3::All"
 
 ### b-tagging ##################################################################
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
@@ -46,23 +46,23 @@ process.impactParameterTagInfos.jetTracks = cms.InputTag("sisCone5JetTracksAssoc
 # parameterization
 if runon=='data':
     source_fileNames = cms.untracked.vstring(
-		# 900 GeV data
-#		'/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0006/5A5773A3-B9ED-DE11-A99A-0026189437F8.root',
-#		'/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0006/CA156CCC-B9ED-DE11-B022-00261894390B.root'
-		'/store/data/BeamCommissioning09/MinimumBias/RECO/Dec19thReReco_341_v1/0006/DA397007-23EE-DE11-A754-00261894388D.root'
-		# 2360 GeV data
-#        '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0005/B641315C-ACED-DE11-82E1-0030486792B6.root',
-#        '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0005/8EB877E1-A5ED-DE11-9680-002618943826.root',
-#        '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0005/52B38DBB-A1ED-DE11-AAB0-00248C0BE014.root',
-    )
+        # 900 GeV data
+        #		'/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0006/5A5773A3-B9ED-DE11-A99A-0026189437F8.root',
+        #		'/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0006/CA156CCC-B9ED-DE11-B022-00261894390B.root'
+        '/store/data/BeamCommissioning09/MinimumBias/RECO/Dec19thReReco_341_v1/0006/DA397007-23EE-DE11-A754-00261894388D.root'
+        # 2360 GeV data
+        #        '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0005/B641315C-ACED-DE11-82E1-0030486792B6.root',
+        #        '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0005/8EB877E1-A5ED-DE11-9680-002618943826.root',
+        #        '/store/data/BeamCommissioning09/MinimumBias/RAW-RECO/SD_InterestingEvents-Dec19thSkim_341_v1/0005/52B38DBB-A1ED-DE11-AAB0-00248C0BE014.root',
+        )
 elif runon=='MC31x':
     source_fileNames = cms.untracked.vstring(
-		'/store/mc/Summer09/LM0/GEN-recoJet_srcSIM-RECO/MC_31X_V3_7TeV-v1/0006/C81C39F6-B4BF-DE11-95E4-001CC47BA216.root'   
-    )
+        'file:/data/14/stiegerb/SUSY/31X/TTbar4Jets_40GeVthreshold-alpgen-GEN-SIM-RECO-MC_31X_V3_7TeV-v3.root'   
+        )
 elif runon=='MC34x':
     source_fileNames = cms.untracked.vstring(
-		'/store/mc/Summer09/MinBias/GEN-SIM-RECO/V16D_900GeV-v1/0001/FCD70794-F216-DF11-931A-0015170AC494.root'
-    )
+        '/store/mc/Summer09/MinBias/GEN-SIM-RECO/V16D_900GeV-v1/0001/FCD70794-F216-DF11-931A-0015170AC494.root'
+        )
 # Input
 process.source = cms.Source("PoolSource",
 	fileNames = source_fileNames,
@@ -73,7 +73,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 # Output
 process.TFileService = cms.Service("TFileService",
 # keep track of the type of data source and reco type in the ntuple file name
-	fileName = cms.string("NTupleProducer_34X_"+runon+"_"+recoType+".root"),  
+	fileName = cms.string("NTupleProducer_34X_"+runon+"_"+recoType+".root"),
 	closeFileFast = cms.untracked.bool(True)
 )
 
@@ -108,38 +108,25 @@ switchJetCollection(process,cms.InputTag(recoJet_src),
                     doType1MET   = True,
                     genJetCollection=cms.InputTag(genJet_src)
                     )
-process.ak5JetID.src = cms.InputTag(recoJet_src)                
+process.ak5JetID.src = cms.InputTag(recoJet_src)
 
 if runon=='data':
-	removeMCMatching(process, 'All')
-	
-	# get the 900 GeV or 2360 GeV jet corrections
-	switchJECSet( process, "900GeV")
-	#switchJECSet( process, "2360GeV")
+    removeMCMatching(process, 'All')
+    # get the 900 GeV or 2360 GeV jet corrections
+    switchJECSet( process, "900GeV")
+    #switchJECSet( process, "2360GeV")
 
-
-### Egamma Isolation ###########################################################
-# Keep in line with non-PAT config.
-process.eleIsoDepositEcalFromHits.ExtractorPSet.barrelEcalHits = "ecalRecHit:EcalRecHitsEB"
-process.eleIsoDepositEcalFromHits.ExtractorPSet.endcapEcalHits = "ecalRecHit:EcalRecHitsEE"
-process.eleIsoFromDepsEcalFromHitsByCrystal.deposits[0].vetos =	cms.vstring('EcalBarrel:0.045', 
-																			'EcalBarrel:RectangularEtaPhiVeto(-0.02,0.02,-0.5,0.5)',
-																			'EcalBarrel:AbsThresholdFromTransverse(0.08)',
-																			'EcalEndcaps:AbsThreshold(0.100)',
-																			'EcalEndcaps:0.070', 
-																			'EcalEndcaps:RectangularEtaPhiVeto(-0.02,0.02,-0.5,0.5)')
-
-# Example configuration
-process.eleIsoFromDepsEcalFromHitsByCrystal.deposits[0].deltaR = 0.4
+# Speed up a bit
+removeCleaning(process,False)
 
 ### Analysis configuration #####################################################
 process.load("DiLeptonAnalysis.NTupleProducer.ntupleproducer_cfi")
 process.analyze.tag_muons     = 'allLayer1Muons'
 process.analyze.tag_electrons = 'allLayer1Electrons'
 process.analyze.tag_jets      = 'allLayer1Jets'
-process.analyze.tag_photons	  = 'allLayer1Photons'
-process.analyze.isPat		= True
-process.analyze.isRealData	= cms.untracked.bool(runon=='data')
+process.analyze.tag_photons   = 'allLayer1Photons'
+process.analyze.isPat         = True
+process.analyze.isRealData    = cms.untracked.bool(runon=='data')
 
 #### Path ######################################################################
 process.mybtag = cms.Sequence(   process.impactParameterTagInfos
@@ -151,4 +138,3 @@ process.p = cms.Path(
     + process.mybtag
     + process.analyze
     )
-
