baef86f318d01bc275607328bb460d57b43327d5
buchmann <>
2011-10-18 09:12:02 +0000

    Avoiding crab error 70500

--- test/crab/SMS_T2/special_run.sh
+++ test/crab/SMS_T2/special_run.sh
@@ -2,8 +2,7 @@
 
 LOG="cmssw"
 scram setup lhapdffull
-touch $CMSSW_BASE/src/DiLeptonAnalysis/NTupleProducer/BuildFile.xml
 eval `scram ru -sh`
 scram b
 
-cmsRun -j crab_fjr.xml ../../ntupleproducer_cfg.py runon=MC ModelScan=True
+cmsRun -j  $RUNTIME_AREA/crab_fjr_$NJob.xml pset.py
