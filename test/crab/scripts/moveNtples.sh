54b3fe97394627bc7151f08fd7d4daa5cc239caf
leo <>
2011-11-01 15:07:17 +0000

    Moves ntples from CSCS to PSI using data_replica.py Run it from the mc_T2 or data_T2 directory Usage: ./moveNtples.sh version crabDir
    changes:
     - added `whoami`

--- test/crab/scripts/moveNtples.sh
+++ test/crab/scripts/moveNtples.sh
@@ -5,8 +5,11 @@
 ###############################################
 
 if [ $# != 2 ]; then
+    echo ""
     echo "Moves ntples from CSCS to PSI using data_replica.py"
+    echo "Run it from the mc_T2 or data_T2 directory"
     echo "Usage: ${0} version crabDir";
+    echo ""
     exit
 fi
 
@@ -15,8 +18,8 @@ DATASET=${2}
 
 
 ### run twice findDuplicates, to get the final purged list
-../scripts/findDuplicates.py --site=t2cscs --refresh --tryDelete /store/user/leo/ntuples/mc/${VERSION}/${DATASET}
-../scripts/findDuplicates.py --site=t2cscs --refresh --tryDelete /store/user/leo/ntuples/mc/${VERSION}/${DATASET}
+../scripts/findDuplicates.py --site=t2cscs --refresh --tryDelete /store/user/`whoami`/ntuples/mc/${VERSION}/${DATASET}
+../scripts/findDuplicates.py --site=t2cscs --refresh --tryDelete /store/user/`whoami`/ntuples/mc/${VERSION}/${DATASET}
 
 ### create a list suitable for data_replica
 cat ${DATASET}.t2cscs.lst | grep '.root' | sed 's:.*\(/store/.*\):\1:g' > ${DATASET}.t2cscs.datareplica.lst
