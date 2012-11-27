d9ea3f6886c5a2b0822433fb7ffda9e67fb7bbd5
mdunser <>
2012-11-27 14:15:21 +0000

    new global tag and example file for run2012D

--- test/ntupleproducer_cfg.py
+++ test/ntupleproducer_cfg.py
@@ -57,7 +57,7 @@ options.register ('perEvtMvaWeights',
 # set NTupleProducer defaults (override the output, files and maxEvents parameter)
 #options.files= 'file:////shome/mdunser/files/isoSynchFile_DoubleMu191700.root'
 #options.files= 'file://///shome/pablom/tmp/newCode/CMSSW_5_2_5_patch1/src/DiLeptonAnalysis/NTupleProducer/A8922572-9D84-E111-88B9-003048F024FE.root'
-options.files= 'file:////shome/haweber/files/TTW_53X.root'
+options.files= 'file:////shome/mdunser/scripts/DoubleMu_Run2012D.root'
 #options.files= 'file:////shome/mdunser/files/JetHT_Run2012C_v1.root'
 #options.files='file:////scratch/fronga/RelValTTbarLepton_EE4E6727-2C7A-E111-A4E8-002354EF3BCE.root'
 
@@ -78,8 +78,9 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
 if options.runon=='data':
 #https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions
+	process.GlobalTag.globaltag = "GR_P_V42_AN2::All" ## Global Tag for Run2012D prompt reco
     # CMSSW_5_3
-    process.GlobalTag.globaltag = "FT_R_53_V6::All" # For Jul13 ReReco of 2012A+B
+    ## process.GlobalTag.globaltag = "FT_R_53_V6::All" # For Jul13 ReReco of 2012A+B
     ### GLOBAL TAG FOR 2012C v1 and v2
     #https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Summary_of_Global_Tags_used_in_o
     # CMSSW_5_2
